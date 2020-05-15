1cc37f382245145181278fe52ae21ea4df25e4a6
sumanthhs29 <51614512+sumanthhs29@users.noreply.github.com>
2020-05-15 22:36:18 +0530

    Update send_mail.py

--- Python_Code/send_mail.py
+++ Python_Code/send_mail.py
@@ -1,9 +1,9 @@
 import smtplib
 s = smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
-s.login("cabtime2020@gmail.com","cab2020time@")
+s.login("senderMail","senderPassword")
 SUBJECT = "Checking"
 TEXT = "Text  is here"
 message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-s.sendmail("cabtime2020@gmail.com","sumanthhs29@gmail.com",message)
+s.sendmail("senderMail","receiverMail",message)
 s.quit()
